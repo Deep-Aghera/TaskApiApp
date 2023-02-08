ac753aca782fa077887173902eac3cc4cef8b364
Deep Aghera <deep.aghera@marutitech.com>
2023-02-08 11:11:33 +0530

    validate update

--- src/index.js
+++ src/index.js
@@ -94,7 +94,15 @@ app.get('/users/:id',async (req,res) => {
 })
 
 app.patch('/user/:id',async (req,res) => {
-    console.log(req.params.id)
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name','email','password','age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({error : "Invalid updates!"});
+    }
+
+
     try {
         const user = await User.findByIdAndUpdate(req.params.id,req.body,{ new : true, runValidators: true });
         if(!user) {
